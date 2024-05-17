682f0035f99f2381a0e297091a68fd55cdd014e6
Phil <phil.miletic@b-it-projects.de>
2024-05-17 16:30:04 +0200

    feat: added pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import { createPinia } from 'pinia'
 
 import { IonicVue } from '@ionic/vue';
 
@@ -34,9 +35,14 @@ import '@ionic/vue/css/palettes/dark.system.css';
 /* Theme variables */
 import './theme/variables.css';
 
+// init pinia
+const pinia = createPinia();
+
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(pinia)
+;
 
 router.isReady().then(() => {
   app.mount('#app');
